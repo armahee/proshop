e4723a7864157d117c07df342f01d7d0c14d6e72
armahee <arifrayhan911@gmail.com>
2022-01-18 10:53:29 +0600

    first upload

---
+++ precache-manifest.33700e4ecda9ac0463f523ff89d4b780.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc7a8faed626cb5011d076c4a646b7b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0965f3d010c4713e8fab",
+    "url": "/static/css/main.a06dfdf4.chunk.css"
+  },
+  {
+    "revision": "7d7a81721a807cd9d867",
+    "url": "/static/js/2.84d1610d.chunk.js"
+  },
+  {
+    "revision": "0a6e30d2c19e290e725e",
+    "url": "/static/js/3.34e8b613.chunk.js"
+  },
+  {
+    "revision": "0965f3d010c4713e8fab",
+    "url": "/static/js/main.c76d39a8.chunk.js"
+  },
+  {
+    "revision": "4b6838e48b89c219e8ac",
+    "url": "/static/js/runtime~main.56c1c384.js"
+  }
+]);
