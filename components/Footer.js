f6b7ce38803e19eb10e3199c3b3441c886c9400a
armahee <arifrayhan911@gmail.com>
2022-01-18 12:04:22 +0600

    Static

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">Copyright &copy; ProShop</Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ index.js
@@ -0,0 +1,10 @@
+import "./index.css";
+import "./bootstrap.min.css";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
