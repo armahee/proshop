131e17dc633dfd1a63817a8bdb8104177d4c4300
armahee <arifrayhan911@gmail.com>
2022-01-19 14:53:27 +0600

    gh-page

---
+++ precache-manifest.ac0b75d724bfe298e9d61bc685af65f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca5f7e4173f095ae67ba741ae4fe789c",
+    "url": "/proshop/index.html"
+  },
+  {
+    "revision": "0965f3d010c4713e8fab",
+    "url": "/proshop/static/css/main.a06dfdf4.chunk.css"
+  },
+  {
+    "revision": "7d7a81721a807cd9d867",
+    "url": "/proshop/static/js/2.84d1610d.chunk.js"
+  },
+  {
+    "revision": "0a6e30d2c19e290e725e",
+    "url": "/proshop/static/js/3.34e8b613.chunk.js"
+  },
+  {
+    "revision": "0965f3d010c4713e8fab",
+    "url": "/proshop/static/js/main.c76d39a8.chunk.js"
+  },
+  {
+    "revision": "05af02b8cfac4ea6f508",
+    "url": "/proshop/static/js/runtime~main.7f21e37f.js"
+  }
+]);
